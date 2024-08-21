c330a2188a50f9b6d0c41f7872538c091750ac7f
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-08-20 21:43:08 -0300

    use live-reload to live reload page

--- app.js
+++ app.js
@@ -2,10 +2,24 @@ const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const passportConfig = require("./passport-config");
+const livereload = require("livereload");
+const connectLivereload = require("connect-livereload");
 
 const app = express();
+const path = require("path");
 const authRoutes = require("./routes/authRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+
+// live-reload
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, "public"));
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+app.use(connectLivereload());
 
 // database
 const sequelize = require("./config/database");
@@ -28,6 +42,8 @@ app.use(
   })
 );
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -36,12 +52,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.locals.user = req.session.user;  // make user data available in views
+  res.locals.user = req.session.user; // make user data available in views
   next();
 });
 
-
-// moutes
+// routes
 app.use("/", messageRoutes);
 app.use("/", authRoutes);
 

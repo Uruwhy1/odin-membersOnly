6a1cb6b859ac2c1a15661e14a9b5c96bae361b4e
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-09-02 14:45:04 -0300

    add welcome message

--- app.js
+++ app.js
@@ -20,8 +20,34 @@ const Message = require("./models/message");
 User.hasMany(Message, { foreignKey: "userId", as: "messages" });
 Message.belongsTo(User, { foreignKey: "userId", as: "user" });
 
-sequelize.sync().then(() => {
+sequelize.sync().then(async () => {
   console.log("Database & tables created!");
+
+  // create user and send test message
+  const existingUser = await User.findOne({
+    where: { email: "fernandoalonso@gmail.com" },
+  });
+
+  if (!existingUser) {
+    const bcrypt = require("bcryptjs");
+    const defaultUser = await User.create({
+      firstName: "Fernando",
+      lastName: "Alonso",
+      email: "fernandoalonso@gmail.com",
+      password: await bcrypt.hash("123456", 10),
+      membershipStatus: true,
+    });
+
+    await Message.create({
+      title: "Welcome Message",
+      text: `If you sign up, you will be able to see message dates. If you become a member (top-right menu), you will be able to see authors. Also, your messages will look different from others'. Please do not try to break this page, it probably will. Oh, and I did not add any media queries or anything, so this might break on lower widths :3`,
+      userId: defaultUser.id,
+    });
+
+    console.log("Default user and message created.");
+  } else {
+    console.log("Default user already exists, skipping creation.");
+  }
 });
 
 const PORT = process.env.PORT || 3000;
